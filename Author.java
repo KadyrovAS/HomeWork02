19f0df6750ce28b6280dadb304a04c85792dcee6
Andrey <kadyrovas@yandex.ru>
2025-03-12 18:39:33 +0300

    Commit with homework Objects

---
+++ Author.java
@@ -0,0 +1,17 @@
+public class Author{
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ LearnObjects.java
@@ -0,0 +1,11 @@
+public class LearnObjects{
+    public static void main(String[] args) {
+        Author authorFirst = new Author("Лев", "Толстой");
+        Author authorSecond = new Author("Фёдор", "Достоевский");
+
+        Book bookFirst = new Book("Война и мир", authorFirst, 1865);
+        Book bookSecond = new Book("Преступление и наказание", authorSecond, 1866);
+
+        bookFirst.setPublicationYear(1924);
+    }
+}
